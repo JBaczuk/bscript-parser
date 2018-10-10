4619cae9cc683b766406a577e3d298d4456ff392
Jordan Baczuk <Jordan.Baczuk@gmail.com>
2018-10-10 00:24:47 +0000

    add OP_RETURN and PUSHDATA support. Fixes #8

--- index.js
+++ index.js
@@ -3,12 +3,24 @@ const rOps = require('bitcoin-ops/map')
 
 let parseRawScriptHex = function (rawScript, parsedScript) {
     let opStr = rawScript.substring(0,2)
-    let op = parseInt(`0x${opStr}`)
+    let op = parseInt(opStr, 16)
     // Custom descriptors (not necessarily op codes)
     if (op > 0 && op < 76) {
         parsedScript += `PUSHDATA(${op})[${rawScript.substring(2, 2+(2*op))}] `
         rawScript = rawScript.substring(2+(2*op))
-    } else {
+    } else if (op === 76) {
+        let bytesToPush = parseInt(rawScript.substring(2, 4), 16)
+        parsedScript += `PUSHDATA(${bytesToPush})[${rawScript.substring(4, 4 + (2 * bytesToPush))}] `
+        rawScript = rawScript.substring(4 + (2 * bytesToPush))
+    } else if (op === 77) {
+        let bytesToPush = parseInt(rawScript.substring(2, 6), 16)
+        parsedScript += `PUSHDATA(${bytesToPush})[${rawScript.substring(6, 6 + (2 * bytesToPush))}] `
+        rawScript = rawScript.substring(6 + (2 * bytesToPush))
+    } else if (op === 78) {
+        let bytesToPush = parseInt(rawScript.substring(2, 8), 16)
+        parsedScript += `PUSHDATA(${bytesToPush})[${rawScript.substring(8, 8 + (2 * bytesToPush))}] `
+        rawScript = rawScript.substring(8 + (2 * bytesToPush))
+    } else { // TODO: 77 and 78
         parsedScript += `${rOps[op]} `
         rawScript = rawScript.substring(2)
     }
@@ -21,7 +33,7 @@ let parseRawScriptHex = function (rawScript, parsedScript) {
 
 // TODO: Could also include a dissasembly https://defuse.ca/online-x86-assembler.htm#disassembly2
 
-let parseRawScript = function (rawScript, format) {
+let parseRawScript = function (rawScript, format='hex') {
     let asm = ''
     switch (format) {
         case 'hex':
