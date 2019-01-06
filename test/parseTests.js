682082774d6864e9f9212ce9d5c578d259357735
Mark Wardle <mwwardle@gmail.com>
2019-01-05 18:25:38 -0700

    Add convenience functions. Implements #23

--- test/parseTests.js
+++ test/parseTests.js
@@ -3,23 +3,34 @@ const script = require('../index.js')
 const conversions = require('./fixtures/conversions.json')
 
 describe('Parse Tests', function () {
-  describe('parseRawScript Tests', function () {
+  describe('rawToAsm Tests', function () {
     conversions.forEach(function (conversion) {
       describe(conversion.name, function () {
         it('returns the expected assembly', function () {
-          const parsedScript = script.parseRawScript(conversion.raw)
+          const parsedScript = script.rawToAsm(conversion.raw)
           expect(parsedScript).to.equal(conversion.asm)
         })
       })
     })
   })
 
-  describe('parseAsmScript Tests', function () {
+  describe('asmToRaw Tests', function () {
     conversions.forEach(function (conversion) {
       describe(conversion.name, function () {
         it('returns the expected raw script', function () {
-          const parsedScript = script.parseAsmScript(conversion.asm, 'hex')
+          const parsedScript = script.asmToRaw(conversion.asm, 'hex')
           expect(parsedScript).to.equal(conversion.raw)
+        })
+      })
+    })
+  })
+
+  describe('formatAsm Tests', function () {
+    conversions.forEach(function (conversion) {
+      describe(conversion.name, function () {
+        it('returns the expected raw script', function () {
+          const parsedScript = script.formatAsm(conversion.asm, 'hex')
+          expect(parsedScript).to.equal(conversion.asm)
         })
       })
     })
