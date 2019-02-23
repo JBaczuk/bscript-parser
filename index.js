5ea696ae4105aa8a9c1c6544f1a9f6d1ea15e0ef
Mark Wardle <mark@potient.com>
2019-02-23 10:22:32 -0700

    Docs for address conversion convenience functions

--- index.js
+++ index.js
@@ -31,6 +31,19 @@ BScript.rawToAsm = function (rawScript, optionsOrEncoding = {}) {
   return BScript.fromRaw(rawScript, encoding).toAsm(options)
 }
 
+/**
+ * Convert raw bscript into an address.
+ *
+ * @static
+ * @param {string|Buffer} rawScript - The raw bscript
+ * @param {string|null|Options} [optionsOrEncoding={}] - An Options object, or just the `encoding` option as a string
+ * @return {string} The raw bscript converted to an address
+ *
+ * @example
+ * const {rawToAddress} = require('bscript-parser')
+ * const address = rawToAddress('a914c664139327b98043febeab6434eba89bb196d1af87', 'hex')
+ * assert.equal(address, '3Kn1bxgQwwqfCQCLtfErFFdqQLydN5t8ez')
+ */
 BScript.rawToAddress = function (rawScript, optionsOrEncoding = {}) {
   let options = optionsOrEncoding
 
@@ -48,6 +61,7 @@ BScript.rawToAddress = function (rawScript, optionsOrEncoding = {}) {
 /**
  * Convert a bscript asm string into raw bscript.
  *
+ * @static
  * @param {string} asmScript - The bscript asm string
  * @param {string|null|Options} [optionsOrEncoding={}] - An Options object or just the encoding option
  * @return {string|Buffer} The asm string converted to raw bscript
@@ -71,10 +85,36 @@ BScript.asmToRaw = function (asmScript, optionsOrEncoding = {}) {
   return BScript.fromAsm(asmScript, options).toRaw(encoding)
 }
 
+/**
+ * Convert a bscript asm string into an address.
+ *
+ * @static
+ * @param {string} asmScript - The bscript asm string
+ * @param {Options} [options={}] - An Options object
+ * @return {string} The asm string converted to an address
+ *
+ * @example
+ * const {asmToAddress} = require('bscript-parser')
+ * const address = asmToAddress('OP_HASH160 c664139327b98043febeab6434eba89bb196d1af OP_EQUAL')
+ * assert.equal(address, '3Kn1bxgQwwqfCQCLtfErFFdqQLydN5t8ez')
+ */
 BScript.asmToAddress = function (asmScript, options = {}) {
   return BScript.fromAsm(asmScript, options).toAddress(options)
 }
 
+/**
+ * Convert an address string into raw bscript.
+ *
+ * @static
+ * @param {string} address - The address
+ * @param {string|null|Options} [optionsOrEncoding={}] - An Options object or just the encoding option
+ * @return {string|Buffer} The address converted to raw bscript
+ *
+ * @example
+ * const {addressToRaw} = require('bscript-parser')
+ * const raw = addressToRaw('3Kn1bxgQwwqfCQCLtfErFFdqQLydN5t8ez', 'hex')
+ * assert.equal(raw, 'a914c664139327b98043febeab6434eba89bb196d1af87')
+ */
 BScript.addressToRaw = function (address, optionsOrEncoding = {}) {
   let options = optionsOrEncoding
 
@@ -89,6 +129,19 @@ BScript.addressToRaw = function (address, optionsOrEncoding = {}) {
   return BScript.fromAddress(address, options).toRaw(encoding)
 }
 
+/**
+ * Convert an address into a bscript asm string.
+ *
+ * @static
+ * @param {string} address - The address
+ * @param {Options} [Options={}] - An Options object
+ * @return {string} The address converted to an asm string
+ *
+ * @example
+ * const {addressToAsm} = require('bscript-parser')
+ * const asm = addressToAsm('3Kn1bxgQwwqfCQCLtfErFFdqQLydN5t8ez')
+ * assert.equal(asm, 'OP_HASH160 c664139327b98043febeab6434eba89bb196d1af OP_EQUAL')
+ */
 BScript.addressToAsm = function (address, options = {}) {
   return BScript.fromAddress(address, options).toAsm(options)
 }
@@ -96,6 +149,7 @@ BScript.addressToAsm = function (address, options = {}) {
 /**
  * Reformat a bscript asm string.
  *
+ * @static
  * @param {string} asmScript - The bscript asm string
  * @param {Options} [options={}] - The options for the reformatted code
  * @return {string} The reformatted asm script.
